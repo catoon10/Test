8f847b1838e3766ac2ff430cb651e7c317d164a7
pluiiklung <pluiiklung@gmail.com>
2016-04-25 19:00:10 +0700

    test_3_1

--- GIT_Test/Assets/ChangeTxt.cs
+++ GIT_Test/Assets/ChangeTxt.cs
@@ -11,7 +11,7 @@ public class ChangeTxt : MonoBehaviour {
 
     void Start()
     {
-        nickname.text = "I love You";
+        nickname.text = "ALOHAAA!!";
         nickname2.text = "Nice to meet you!!";
     }
     
