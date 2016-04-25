053914a65d127d41386dda58bd67a5a012025c9a
Toon <chayut400@gmail.com>
2016-04-25 17:06:21 +0700

    V2
    Toon

---
+++ GIT_Test/Assets/ChangeTxt.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class ChangeTxt : MonoBehaviour {
+
+    public Text nickname;
+
+    void Start()
+    {
+        nickname.text = "Toon";
+    }
+    
+    
+    
+}
